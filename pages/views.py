0d1e9c2b8c5c9c98d6c0afb5ccc362468cdfc929
Maria Kennedy <mariakingsbury1@gmail.com>
2024-04-30 00:35:13 -0500

    added page templates,urls, and views for each static page

--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request): 
     return render(request, 'pages/home.html')
+def about(request):
+    return render(request, 'pages/about.html')
+def work(request):
+    return render(request, 'pages/work.html')
+def contact(request):
+    return render(request, 'pages/contact.html')
